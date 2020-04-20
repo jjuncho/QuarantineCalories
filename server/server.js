6c9a159b53854c550ced66c297d90d37b1e14ae4
jjuncho <jjune.oxen@gmail.com>
2020-04-19 20:29:29 -0400

    added api support

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const app = express();
 
+const app = express();
+const APP_ID = process.env.APP_ID;
+const API_KEY = process.env.API_KEY;
+console.log(process.env.APP_ID);
+console.log(process.env.API_KEY);
 var corsOptions = {
   origin: "http://localhost:3000"
 };
@@ -18,8 +23,37 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Placement for hame page" });
+  res.send('Hello World!');
 });
+
+app.get("/totalCalories",(req,res) =>{
+	var request = require('request');
+	var options = {
+  'method': 'POST',
+  'url': 'https://trackapi.nutritionix.com/v2/natural/nutrients',
+  'headers': {
+    'x-app-id': APP_ID ,
+    'x-app-key': API_KEY,
+    'x-remote-user-id': '0',
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({"query":"grilled cheese"})
+
+};
+request(options, function (error, response) { 
+  if (error) throw new Error(error);
+  const result= JSON.parse(response.body);
+  console.log(result.foods[0].nf_calories);
+});
+
+	res.send('5000');
+
+
+
+});
+
+
+
 
 // set port, listen for requests
 const PORT = process.env.PORT || 3000;
