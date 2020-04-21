96aceab3bef512d0993dc75bbd3c65397c16c46d
Mariana Dematte <mdematte@bu.edu>
2020-04-20 23:51:20 -0400

    Added bootstrap, changed project to make it easier to work with bootstrap

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Adding bootstrap to the project
+// See https://bootstrap-vue.js.org/ for inormation and docs
+import { BootstrapVue } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
 
 new Vue({
   render: h => h(App),
