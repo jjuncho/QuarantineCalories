8e63fc9e537233e4beb668020f4c0ab7f2a448dc
jjuncho <jjune.oxen@gmail.com>
2020-04-28 18:37:42 -0400

    added readme

--- server/server.js
+++ server/server.js
@@ -34,7 +34,11 @@ app.use(session({
 	cookie : {maxAge: 60000}
 }));
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 mongoose.Promise = global.Promise;
